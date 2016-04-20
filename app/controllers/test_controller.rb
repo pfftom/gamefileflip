6a2f90bf4c18da63a055423ba8f385531ef345f4
Thomas Maney <tjm036@gmail.com>
2016-04-20 17:44:12 -0400

    a

---
+++ app/controllers/test_controller.rb
@@ -0,0 +1,6 @@
+class TestController < ApplicationController
+
+  def sample
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "index#index"
   
   post "index/uploadfile", to: "index#uploadfile"
+
+  get "/test", to: "test#sample"
 end
